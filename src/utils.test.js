9f3ff70448cc3c8ef8be16651f517e1c000d1bde
Christof Leuenberger <chris@chlab.ch>
2017-11-26 16:15:15 +0100

    Fix resolve unit test
    Process isn’t a module, we have to override process.cwd() globally

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,9 +1,7 @@
 describe('Utils', () => {
   test('Path is resolved correctly', () => {
-    // mock process.cwd
-    jest.mock('process', () => ({
-      cwd: () => '/absolute/path'
-    }))
+    // override process.cwd
+    process.cwd = () => '/absolute/path'
 
     const { resolve } = require('./utils')
     expect(resolve('relative/path')).toBe('/absolute/path/relative/path')
